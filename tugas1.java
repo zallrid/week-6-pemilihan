6c3c02fc3c7cf9244d5c07c746c311e2f0bf9999
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-10-08 10:35:51 +0700

    Tugas 1

--- tugas1.java
+++ tugas1.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class tugas1 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int angka;
+
+        System.out.println("Masukkan angka: ");
+        angka = sc.nextInt();
+
+        if (angka % 2 ==0) {
+            System.out.println("Genap");
+
+        }
+        else {
+            System.out.println("Ganjil");
+        }
+
     }
 }
